571aaaa97164459307085a25f9d682895c79b2bb
jh3r <jay.m.hu@newegg.com>
2018-07-10 16:48:24 +0800

    Add demo

--- examples/counter/src/App.js
+++ examples/counter/src/App.js
@@ -1,6 +1,59 @@
 import React, { Component } from 'react';
 
+function respondData(data, self) {
+  Object.defineProperty(data, 'count', {
+    get() {
+      return this._count;
+    },
+    set(v) {
+      this._count = v;
+      self.forceUpdate();
+    }
+  });
+}
+
 class App extends Component {
+  data = {
+    _count: 0,
+    count: 0
+  };
+
+  constructor(props) {
+    super(props);
+    console.log(this.context, this.props, this.state);
+    this.add = this.add.bind(this);
+    respondData(this.data, this);
+  }
+
+  componentDidCatch() {
+    console.error('componentDidCatch');
+  }
+  componentDidMount() {
+    console.error('componentDidMount');
+  }
+  componentDidUpdate() {
+    console.error('componentDidUpdate');
+  }
+  componentWillMount() {
+    console.error('componentWillMount');
+  }
+  componentWillReceiveProps() {
+    console.error('componentWillReceiveProps');
+  }
+  componentWillUnmount() {
+    console.error('componentWillUnmount');
+  }
+  componentWillUpdate() {
+    console.error('componentWillUpdate');
+  }
+  shouldComponentUpdate() {
+    console.error('shouldComponentUpdate');
+  }
+
+  add() {
+    this.data.count += 1;
+  }
+
   render() {
     return (
       <div className="App">
@@ -8,7 +61,10 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Current Value <span>{this.data.count}</span>
+        </p>
+        <p>
+          <button onClick={this.add}>+1</button>
         </p>
       </div>
     );
--- examples/counter/src/index.js
+++ examples/counter/src/index.js
@@ -3,8 +3,10 @@ import { Provider } from '../../../src';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const store = { a: 1 };
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
