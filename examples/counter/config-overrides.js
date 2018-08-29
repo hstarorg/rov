c515572a900dc88c1373441720aa5045c81b80cd
jh3r <jay.m.hu@newegg.com>
2018-08-29 16:00:17 +0800

    Support decrator

--- examples/counter/config-overrides.js
+++ examples/counter/config-overrides.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const { injectBabelPlugin } = require('react-app-rewired');
 
 module.exports = function override(config, env) {
+  config = injectBabelPlugin(['transform-decorators-legacy'], config);
   const plugin = config.resolve.plugins[0];
   plugin.appSrc = path.join(plugin.appSrc, '../../../');
   const rule0 = config.module.rules[0];
